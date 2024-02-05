feda68b07046dac89fd51c2df4397a189bc7dd8d
WeiFeng <2861663679@qq.com>
2024-02-05 20:33:45 +0800

    first commit

--- src/main/java/com/atwfhome/maven/HelloGitte.java
+++ src/main/java/com/atwfhome/maven/HelloGitte.java
@@ -3,5 +3,6 @@ package com.atwfhome.maven;
 public class HelloGitte {
     public static void main(String[] args) {
         System.out.println("hello gitte");
+	System.out.println("hello world");
     }
 }
